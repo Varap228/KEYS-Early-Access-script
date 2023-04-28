8ee140916b8359015b6e9a8a027926da1246e002
Varap228 <99797025+Varap228@users.noreply.github.com>
2023-04-28 22:30:12 +0300

    fixed bugs

--- code.lua
+++ code.lua
@@ -19,50 +19,39 @@ local Tab_V = Window:NewTab("Visuals")
 local Section_V = Tab_V:NewSection("Visuals")
 ESP:Toggle(true) 
 ESP.Players = false 
+getgenv().espMonster = false
+getgenv().espD = false
+getgenv().ItmEsp = false
+getgenv().setgtpdor = false
 Section:NewButton("Remov invis parts", "Some bug in the School", function()
 	delIPart()
 end)
 Section:NewKeybind("remove gui", "hides the script on the button", Enum.KeyCode.B, function()
 	Library:ToggleUI()
 end)
-Section_E:NewToggle("Esp (some bugs)", "There are mistakes", function(a)
-    if a then
-        _G.espp = true;
-		ESP.Boxes = true
-		ESP.Names = true
-	while _G.espp == true do
-	esp()
-    wait(1.5)
+Section_E:NewToggle("Esp (some bugs)", "There are mistakes", function(espVal)
+	getgenv().espMonster = espVal
+	ESP.Boxes = espVal
+	ESP.Names = espVal
+	if espVal then
+		esp()
 	end
-    else
-        _G.espp = false;
-		ESP.Boxes = false
-		ESP.Names = false
-    end
 end)
-Section_E:NewToggle("Esp wit photo (some bugs)", "There are mistakes", function(a)
-    if a then
-        _G.espp = true;
-	while _G.espp == true do
-	espDecal()
-    wait(1.5)
-	end
-    else
-        _G.espp = false;
+Section_E:NewToggle("Esp wit photo (some bugs)", "There are mistakes", function(espDecalV)
+	getgenv().espD = espDecalV
+	if espDecalV then
+		espDecal()
+	else
 		espDecalDell()
-    end
-end)
-Section_E:NewToggle("Item Esp (some bugs)", "There are mistakes", function(a)
-    if a then
-        _G.espp = true;
-	while _G.espp == true do
-	Iesp()
-    wait(1.5)
 	end
-    else
-        _G.espp = false;
+end)
+Section_E:NewToggle("Item Esp (some bugs)", "There are mistakes", function(ItmEspV) 
+	getgenv().ItmEsp = ItmEspV
+	if ItmEspV then
+		Iesp()
+	else
 		IespDell()
-    end
+	end
 end)
 Section_D:NewToggle("Win door ", "Only already open", function(a)
     if a then
@@ -83,23 +72,22 @@ Section_D:NewKeybind("Win door check", "Only already open", Enum.KeyCode.Z, func
         local victoryDoorFound = false
         for _, v in ipairs(itemHuntFolder:GetDescendants()) do
             if v.Name == "TouchInterest" and v.Parent then
-                if _G.TpD == true then
+				if getgenv().setgtpdor then
                     local back = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
                     wait(1)
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 4, -50)
-                    wait(2.55)
+                    wait(3)
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
                     victoryDoorFound = true
-                    wait(2)
+                    wait(3)
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = back
                     break
-                else
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
-                    victoryDoorFound = true
-                    break
+				else
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.CFrame
+					break
+				end	
                 end
             end
-        end
         return victoryDoorFound
     end
 
@@ -114,14 +102,9 @@ Section_D:NewKeybind("Win door check", "Only already open", Enum.KeyCode.Z, func
             victoryDoorNotFound = true
         end
     end
-
-    if victoryDoorNotFound then
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "No win door",
-            Text = "while there is no victory door ",
-            Icon = ""
-        })
-    end
+end)
+Section_D:NewToggle("tp lobby then to win door (This is the setting)", "", function(a)
+	getgenv().setgtpdor = a
 end)
 Section_D:NewKeybind("Tp to key", "", Enum.KeyCode.X, function()
 	tpkey()
@@ -148,7 +131,7 @@ Section_S:NewButton("Tp to lobby", "", function()
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Lobby.BackLobbySpawnLocation.CFrame + Vector3.new(0,3,0)
 end)
 Section_S:NewButton("Tp to back", "", function()
-	if _G.tpback == "nil" then
+	if _G.tpback == nil then
 		game:GetService("StarterGui"):SetCore("SendNotification",{
 		Title = "Tp back nil",
 		Text = "first tp in the lobby", 
@@ -242,11 +225,6 @@ Section_cred:NewButton("Change log", "", function()
 end)
 function delIPart()
 	local Maps = game:GetService("Workspace").Maps
-	Maps.School.InvisParts:Destroy()
-	Maps.Castle.InvisParts:Destroy()
-	Maps.MagicCube.InvisParts:Destroy()
-	Maps.Refuge.InvisParts:Destroy()
-
 	for _, v in ipairs(Maps.Refuge.Parts["3"]:GetDescendants()) do
 		if v.Name == "collide" then
 			v:Destroy()
@@ -258,6 +236,10 @@ function delIPart()
 			b:Destroy()
 		end
 	end
+	Maps.School.InvisParts:Destroy()
+	Maps.Castle.InvisParts:Destroy()
+	Maps.MagicCube.InvisParts:Destroy()
+	Maps.Refuge.InvisParts:Destroy()
 end
 function panic()
 	if game:GetService("Players").Gejoop28.PlayerGui.ScreenChasedWarning.Enabled then
@@ -332,41 +314,46 @@ function espDecalDell()
 end
 end
 function espDecal()
-	local function createBillboardGui(b, imageId)
-	local billboardGui = b:FindFirstChild("BillboardGui")
-	if not billboardGui then
-		billboardGui = Instance.new("BillboardGui")
-		billboardGui.Adornee = b
-		billboardGui.Size = UDim2.new(0, 55, 0, 55)
-		billboardGui.AlwaysOnTop = true
-		billboardGui.StudsOffset = Vector3.new(0, 3, 0)
-		local imageLabel = Instance.new("ImageLabel")
-		imageLabel.Parent = billboardGui
-		imageLabel.BackgroundTransparency = 1
-		imageLabel.Size = UDim2.new(1, 0, 1, 0)
-		billboardGui.Parent = b
-	end
-	local imageLabel = billboardGui.ImageLabel
-	imageLabel.Image = "rbxassetid://" .. tostring(imageId)
-	billboardGui.Enabled = true
-	end
-	local gamePlayers = game:GetService("Workspace").Game.GamePlayers:GetChildren()
-	for i = 1, #gamePlayers do
-	local b = gamePlayers[i]
-	local imageName
-	if b.Name == "Butcher" then
-		imageName = 12629402865
-	elseif b.Name == "Bear" then
-		imageName = 12629403201
-	elseif b.Name == "Rabbit" then
-		imageName = 12629403353
-	elseif b.Name == "Joker" then
-		imageName = 12629403068
-	end
-	if imageName then
-		createBillboardGui(b, imageName)
-	end
-	end
+	spawn(function ()
+		while getgenv().espD == true do
+			local function createBillboardGui(b, imageId)
+			local billboardGui = b:FindFirstChild("BillboardGui")
+			if not billboardGui then
+				billboardGui = Instance.new("BillboardGui")
+				billboardGui.Adornee = b
+				billboardGui.Size = UDim2.new(0, 55, 0, 55)
+				billboardGui.AlwaysOnTop = true
+				billboardGui.StudsOffset = Vector3.new(0, 3, 0)
+				local imageLabel = Instance.new("ImageLabel")
+				imageLabel.Parent = billboardGui
+				imageLabel.BackgroundTransparency = 1
+				imageLabel.Size = UDim2.new(1, 0, 1, 0)
+				billboardGui.Parent = b
+			end
+			local imageLabel = billboardGui.ImageLabel
+			imageLabel.Image = "rbxassetid://" .. tostring(imageId)
+			billboardGui.Enabled = true
+			end
+			local gamePlayers = game:GetService("Workspace").Game.GamePlayers:GetChildren()
+			for i = 1, #gamePlayers do
+			local b = gamePlayers[i]
+			local imageName
+			if b.Name == "Butcher" then
+				imageName = 12629402865
+			elseif b.Name == "Bear" then
+				imageName = 12629403201
+			elseif b.Name == "Rabbit" then
+				imageName = 12629403353
+			elseif b.Name == "Joker" then
+				imageName = 12629403068
+			end
+			if imageName then
+				createBillboardGui(b, imageName)
+			end
+			end
+			wait(1.5)
+		end
+	end)
 end
 function IespDell()
 	local function disableBillboardGuis(folder)
@@ -386,60 +373,70 @@ function IespDell()
 	disableBillboardGuis(refugeFolder)
 end
 function Iesp()
-	local function createBillboardGui(parent)
-    local BillboardGui = Instance.new("BillboardGui")
-    local TextLabel = Instance.new("TextLabel")
-    BillboardGui.Parent = parent
-    BillboardGui.AlwaysOnTop = true
-    BillboardGui.LightInfluence = 1
-    BillboardGui.Size = UDim2.new(0, 50, 0, 50)
-    BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-    TextLabel.Parent = BillboardGui
-    TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-    TextLabel.BackgroundTransparency = 1
-    TextLabel.Size = UDim2.new(1, 0, 1, 0)
-    TextLabel.Text = "Key"
-    TextLabel.TextColor3 = Color3.new(1, 0, 0)
-    TextLabel.TextScaled = true
-	end 
-	local maps = {"Castle", "School", "MagicCube", "Refuge"}
-	for _, map in ipairs(maps) do
-    for i, v in pairs(game.Workspace.Maps[map].ItemHuntFolder.ItemSpawn:GetDescendants()) do
-        if v.Parent.Name == "Handle" and not v.Parent:FindFirstChild("BillboardGui") then
-            createBillboardGui(v.Parent)
-   		end
-	end
-	end
+	spawn(function ()
+		while getgenv().ItmEsp == true do
+			local function createBillboardGui(parent)
+			local BillboardGui = Instance.new("BillboardGui")
+			local TextLabel = Instance.new("TextLabel")
+			BillboardGui.Parent = parent
+			BillboardGui.AlwaysOnTop = true
+			BillboardGui.LightInfluence = 1
+			BillboardGui.Size = UDim2.new(0, 50, 0, 50)
+			BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
+			TextLabel.Parent = BillboardGui
+			TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+			TextLabel.BackgroundTransparency = 1
+			TextLabel.Size = UDim2.new(1, 0, 1, 0)
+			TextLabel.Text = "Key"
+			TextLabel.TextColor3 = Color3.new(1, 0, 0)
+			TextLabel.TextScaled = true
+			end 
+			local maps = {"Castle", "School", "MagicCube", "Refuge"}
+			for _, map in ipairs(maps) do
+				for i, v in pairs(game.Workspace.Maps[map].ItemHuntFolder.ItemSpawn:GetDescendants()) do
+					if v.Parent.Name == "Handle" and not v.Parent:FindFirstChild("BillboardGui") then
+						createBillboardGui(v.Parent)
+					end
+				end
+			end
+			wait(1.5)
+		end
+	end)
 end
 function esp()
-	ESP:AddObjectListener(Workspace.Game.GamePlayers, {
-    Name = "Bear", 
-    CustomName = "Bear",
-    Color = Color3.fromRGB(0, 0, 238), 
-    IsEnabled = "Bear1" 
-	})
-	ESP:AddObjectListener(Workspace.Game.GamePlayers, {
-    Name = "Butcher", 
-    CustomName = "Butcher",
-    Color = Color3.fromRGB(238, 173, 14), 
-    IsEnabled = "Butcher1" 
-	})
-	ESP:AddObjectListener(Workspace.Game.GamePlayers, {
-    Name = "Joker", 
-    CustomName = "Joker",
-    Color = Color3.fromRGB(255, 0, 0), 
-    IsEnabled = "Joker1" 
-	})
-	ESP:AddObjectListener(Workspace.Game.GamePlayers, {
-    Name = "Rabbit", 
-    CustomName = "Rabbit",
-    Color = Color3.fromRGB(255,105,180), 
-    IsEnabled = "Rabbit1" 
-	})
-	ESP.Bear1 = true
-	ESP.Butcher1 = true
-	ESP.Joker1 = true
-	ESP.Rabbit1 = true
+	spawn(function ()
+		while getgenv().espMonster == true do
+			ESP:AddObjectListener(Workspace.Game.GamePlayers, {
+			Name = "Bear", 
+			CustomName = "Bear",
+			Color = Color3.fromRGB(0, 0, 238), 
+			IsEnabled = "Bear1" 
+			})
+			ESP:AddObjectListener(Workspace.Game.GamePlayers, {
+			Name = "Butcher", 
+			CustomName = "Butcher",
+			Color = Color3.fromRGB(238, 173, 14), 
+			IsEnabled = "Butcher1" 
+			})
+			ESP:AddObjectListener(Workspace.Game.GamePlayers, {
+			Name = "Joker", 
+			CustomName = "Joker",
+			Color = Color3.fromRGB(255, 0, 0), 
+			IsEnabled = "Joker1" 
+			})
+			ESP:AddObjectListener(Workspace.Game.GamePlayers, {
+			Name = "Rabbit", 
+			CustomName = "Rabbit",
+			Color = Color3.fromRGB(255,105,180), 
+			IsEnabled = "Rabbit1" 
+			})
+			ESP.Bear1 = true
+			ESP.Butcher1 = true
+			ESP.Joker1 = true
+			ESP.Rabbit1 = true
+			wait(1.5)
+		end
+	end)
 end
 function Door()
 	local function sendNotification(title, text, icon)
@@ -451,6 +448,8 @@ function Door()
 	end
 	local function teleportToDoor(doorCFrame)
 	if _G.dtp == true then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 4, -50)
+		wait(5)
 		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = doorCFrame
 	end
 	end
